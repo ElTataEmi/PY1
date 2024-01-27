7946ae31ae81fc549035fc22241a2fe9678e2b5b
ElTataEmi <turasis88@gmail.com>
2024-01-27 15:02:22 -0300

    Se agregan rutas al Home

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname,'./views/home.html'))
 })
 
-app.get("registro", (req, res) => {
+app.get("/register", (req, res) => {
     res.sendFile(path.join(__dirname,'./views/register.html'))
 })
 
