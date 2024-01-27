35df80ace0985491f1c08111eca30c338fb7514c
ElTataEmi <turasis88@gmail.com>
2024-01-27 14:41:50 -0300

    Se crean Vistas

--- app.js
+++ app.js
@@ -7,10 +7,27 @@ const port = 3030;
 
 app.use(express.static('public'))
 
+app.use(express.static('desing'))
+
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname,'./views/home.html'))
 })
 
+app.get("registro", (req, res) => {
+    res.sendFile(path.join(__dirname,'./views/register.html'))
+})
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname,'./views/login.html'))
+})
+
+app.get("/carrito", (req, res) => {
+    res.sendFile(path.join(__dirname,'./views/carrito.html'))
+})
+
+app.get("/producto", (req, res) => {
+    res.sendFile(path.join(__dirname,'./views/producto.html'))
+})
 
 app.listen(port, () => console.log(`http://localhost:${port}`))
